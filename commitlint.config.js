ffe11b866a6d416c4943ba45218ece40c3fd5b80
paulChess <sjq15251852316@163.com>
2022-05-03 02:58:11 +0800

    测试

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   extends: ['cz'],
-  // parserPreset: {
-  //   parserOpts: {
-  //     headerPattern: /^(.*?)((.*?)):\s?(.*)$/,
-  //     headerCorrespondence: ['type', 'scope', 'subject'],
-  //   },
-  // },
-  // rules: {
-  //   'type-empty': [2, 'never'],
-  //   'subject-empty': [2, 'never'],
-  // },
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(.*?)((.*?)):\s?(.*)$/,
+      headerCorrespondence: ['type', 'scope', 'subject'],
+    },
+  },
+  rules: {
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+  },
 };
