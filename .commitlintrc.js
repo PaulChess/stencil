541bc7b29d301c4b4a3927dd63a48d5a641f04ba
paulChess <sjq15251852316@163.com>
2022-05-03 10:16:55 +0800

    feat(组件): 修改 commit 配置文件
    详细说明
    
    ISSUES CLOSED: #686

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,11 +1,5 @@
 module.exports = {
   extends: ["cz"],
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(.*?)((.*?)):\s?(.*)$/,
-      headerCorrespondence: ['type', 'scope', 'subject'],
-    },
-  },
   rules: {
     'type-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
